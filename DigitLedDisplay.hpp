818c1a87d9bddf1a0f2b77530a2bc6ae92e1b019
MartyVos <31736433+MartyVos@users.noreply.github.com>
2018-11-05 16:03:01 +0100

    Updated library

--- DigitLedDisplay.hpp
+++ DigitLedDisplay.hpp
@@ -1,9 +1,23 @@
-#ifndef DIGITLEDDISPLAY_HPP
-#define DIGITLEDDISPLAY_HPP
+#ifndef DIGIT_LED_DISPLAY_HPP
+#define DIGIT_LED_DISPLAY_HPP
+
+#include "hwlib_TM1637.hpp"
+#include "hwlib.hpp"
 
 class DigitLedDisplay{
+private:
+    int firstDigit;
+    int secondDigit;
+    hwlib::target::pin_in_out CLK;
+    hwlib::target::pin_in_out DIO;
+    TM1637Display segDisplay;
+
 public:
-	void Display(int firstDigit, int secondDigit);
+    DigitLedDisplay(hwlib::target::pin_in_out &CLK,hwlib::target::pin_in_out &DIO):
+        CLK(CLK),DIO(DIO),
+        segDisplay(CLK,DIO)
+    {segDisplay.setBrightness(4);}
+    void Display(int firstDisplay,int secondDisplay);
 };
 
-#endif
+#endif // DIGIT_LED_DISPLAY_HPP
