dfe4c8fdb7cd6b4ac47441010f04739685aa5fa5
MartyVos <31736433+MartyVos@users.noreply.github.com>
2018-11-07 17:14:50 +0100

    Added Doxygen Documentation

--- lib/DigitLedDisplay.hpp
+++ lib/DigitLedDisplay.hpp
@@ -4,20 +4,36 @@
 #include "hwlib_TM1637.hpp"
 #include "hwlib.hpp"
 
+///@class DigitLedDisplay
+///@brief A class for the 4x 7-segment display.
 class DigitLedDisplay{
 private:
     int firstDigit;
     int secondDigit;
+    int brightness;
     hwlib::target::pin_in_out CLK;
     hwlib::target::pin_in_out DIO;
     TM1637Display segDisplay;
 
 public:
-    DigitLedDisplay(hwlib::target::pin_in_out &CLK,hwlib::target::pin_in_out &DIO):
-        CLK(CLK),DIO(DIO),
+    ///@fn DigitLedDisplay::DigitLedDisplay(hwlib::target::pin_in_out &CLK, hwlib::target::pin_in_out &DIO, int brightness = 4)
+    ///@brief The constructor for the DigitLedDisplay class.
+    ///@details This constructor creates a DigitLedDisplay object. 
+    ///@param CLK The clock pin of the display.
+    ///@param DIO The data I/O pin of the display.
+    ///@param brightness Sets the brightness of the display. Default value 4. 
+    DigitLedDisplay(hwlib::target::pin_in_out &CLK, hwlib::target::pin_in_out &DIO, int brightness = 4):
+        brightness(brightness),CLK(CLK),DIO(DIO),
         segDisplay(CLK,DIO)
-    {segDisplay.setBrightness(4);}
-    void Display(int firstDisplay,int secondDisplay);
+    {
+        segDisplay.setBrightness(brightness);
+    }
+    ///@fn void DigitLedDisplay::Display(int firstDigit, int secondDigit)
+    ///@brief The display function.
+    ///@details This function displays the given numbers on the display.
+    ///@param firstDigit Controls the two leftmost digits of the display.
+    ///@param secondDigit Controls the two rightmost digits of the display.
+    void Display(int firstDigit, int secondDigit);
 };
 
 #endif // DIGIT_LED_DISPLAY_HPP
