e79897e63ef08422579e5d1fb7c7060b3aa1d79e
MartyVos <31736433+MartyVos@users.noreply.github.com>
2018-11-08 13:08:10 +0100

    Updated and added new methods

--- lib/DigitLedDisplay.cpp
+++ lib/DigitLedDisplay.cpp
@@ -1,6 +1,24 @@
 #include "DigitLedDisplay.hpp"
 
-void DigitLedDisplay::Display(int firstDigit,int secondDigit){
-    segDisplay.showNumberDecEx(firstDigit,0x40,true,2,0);
+void DigitLedDisplay::Display(int firstDigit,int secondDigit,bool leadingZero){
+    segDisplay.showNumberDecEx(firstDigit,0x40,leadingZero,2,0);
     segDisplay.showNumberDecEx(secondDigit,0x40,true,2,2);
+}
+
+void DigitLedDisplay::ChangeBrightness(int brightness){
+    segDisplay.setBrightness(brightness);
+}
+
+void DigitLedDisplay::TurnDisplayOn(bool newDisplayState){
+    if(isDisplayOn && !newDisplayState){
+        segDisplay.setBrightness(brightness, newDisplayState);
+        isDisplayOn = false;
+    }else if(!isDisplayOn && newDisplayState){
+        segDisplay.setBrightness(brightness, newDisplayState);
+        isDisplayOn = true;
+    }
+}
+
+bool DigitLedDisplay::GetDisplayState(){
+    return isDisplayOn;
 }
--- lib/DigitLedDisplay.hpp
+++ lib/DigitLedDisplay.hpp
@@ -5,12 +5,14 @@
 #include "hwlib.hpp"
 
 ///@class DigitLedDisplay
-///@brief A class for the 4x 7-segment display.
+///@brief A class for the 4x 7-segment display boundary object.
 class DigitLedDisplay{
 private:
     int firstDigit;
     int secondDigit;
     int brightness;
+    bool leadingZero;
+    bool isDisplayOn = false;
     hwlib::target::pin_in_out CLK;
     hwlib::target::pin_in_out DIO;
     TM1637Display segDisplay;
@@ -21,19 +23,36 @@ public:
     ///@details This constructor creates a DigitLedDisplay object. 
     ///@param CLK The clock pin of the display.
     ///@param DIO The data I/O pin of the display.
-    ///@param brightness Sets the brightness of the display. Default value 4. 
-    DigitLedDisplay(hwlib::target::pin_in_out &CLK, hwlib::target::pin_in_out &DIO, int brightness = 4):
-        brightness(brightness),CLK(CLK),DIO(DIO),
+    DigitLedDisplay(hwlib::target::pin_in_out &CLK, hwlib::target::pin_in_out &DIO):
+        CLK(CLK),DIO(DIO),
         segDisplay(CLK,DIO)
-    {
-        segDisplay.setBrightness(brightness);
-    }
+    {}
     ///@fn void DigitLedDisplay::Display(int firstDigit, int secondDigit)
-    ///@brief The display function.
-    ///@details This function displays the given numbers on the display.
+    ///@brief The display method.
+    ///@details This method displays the given numbers on the display.
     ///@param firstDigit Controls the two leftmost digits of the display.
     ///@param secondDigit Controls the two rightmost digits of the display.
-    void Display(int firstDigit, int secondDigit);
+    ///@param leadingZero If @c firstDigit is less than 10, a zero wil be placed at the leftmost position
+    /// if leadingZero equals @c true. Default value is @c true.
+    void Display(int firstDigit, int secondDigit, bool leadingZero = true);
+    
+    ///@fn void DigitLedDisplay::ChangeBrightness(int brightness)
+    ///@brief Change brightness method.
+    ///@details This method changes the brightness of the display.
+    ///@param brightness A number from 0 (lowest brightness) to 7 (highest brightness).
+    void ChangeBrightness(int brightness);
+    
+    ///@fn void DigitLedDisplay::TurnDisplayOn(bool newDisplayState)
+    ///@brief Turns the display on.
+    ///@details This methods turns the display on or off.
+    ///@param newDisplayState Turns the display on (True) or off (False).
+    void TurnDisplayOn(bool newDisplayState);
+    
+    ///@fn bool DigitLedDisplay::GetDisplayState()
+    ///@brief Returns the state of the display.
+    ///@details This method returns @c true if the display is on and returns @c false if the display os off.
+    ///@return isDisplayOn
+    bool GetDisplayState();
 };
 
 #endif // DIGIT_LED_DISPLAY_HPP
